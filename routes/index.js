fc75ea5286e7260448bf8550668b7341e70ae5b5
klim1812 <klimat1812@gmail.com>
2025-05-05 21:47:29 +0300

    05.05.2

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const sensorController = require('../controllers/sensorController');
 
 router.get('/ref1', sensorController.getRef1);
+router.get('/ref2', sensorController.getRef2);
 
 // router.post('/post', sensorController.createData)
 
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const sensorController = require('../controllers/sensorController');
 
-router.get('/ref2', sensorController.getRef2);
+// router.get('/ref2', sensorController.getRef2);
 
 
 module.exports = router;
